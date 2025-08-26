4719bf3781de965cdcb2870cba2fcb8fc5c9d92d
jamesjoel <james.joelj@gmail.com>
2025-08-26 18:43:13 +0530

    project setup

--- project/config/config.js
+++
@@ -1,3 +0,0 @@
-const DB_URL = "mongodb+srv://jamessteppingstone:RcqeDYUOzKtzHEt0@cluster0.juoapks.mongodb.net/";
-const PORT = 3000;
-export {DB_URL, PORT}
--- project/routes/UserRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import {SaveUser, GetAllUser} from '../controllers/UserController.js'
-
-let routes = express.Router();
-
-routes.get("/", GetAllUser)
-routes.post("/", SaveUser)
-
-export default routes;
